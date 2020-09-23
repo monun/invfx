232baa80cff4e4b68a15834e15da84b606e3f956
Noonmaru <noonmaru10@gmail.com>
2020-09-24 00:38:36 +0900

    Update paper version to 1.16.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 dependencies {
     compileOnly(kotlin("stdlib-jdk8"))
     compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
-    compileOnly("com.destroystokyo.paper:paper-api:1.16.2-R0.1-SNAPSHOT")
+    compileOnly("com.destroystokyo.paper:paper-api:1.16.3-R0.1-SNAPSHOT")
 
     implementation("com.github.noonmaru:tap:3.1.1")
     implementation("com.github.noonmaru:kommand:0.3.1")
